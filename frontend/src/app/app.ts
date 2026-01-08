6f4c1219bf4783e3cb9029e203c776cfae1a4e68
alban <alban.haka@stud.unifi.com>
2026-01-08 17:01:19 +0100

    update app structure and add HTTP client support

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+import { TaskService } from './services/task';
+import { TaskResponseDTO } from './models/task.model';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, 
+  imports: [CommonModule, RouterOutlet], 
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class AppComponent implements OnInit { 
+  tasks: TaskResponseDTO[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.loadTasks();
+  }
+
+  loadTasks() {
+    this.taskService.getTasks().subscribe({
+      next: (data) => this.tasks = data,
+      error: (err) => console.error('error loading task', err)
+    });
+  }
 }
