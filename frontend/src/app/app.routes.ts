eb790d1b92cb2e6fc0949c307bbf6a3ab397c7e4
alban <alban.haka@stud.unifi.com>
2026-01-11 18:42:46 +0100

    Add auth guard to protect task routes and implement unit tests

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,17 +4,18 @@ import { RegisterComponent } from './components/register/register';
 import { TaskListComponent } from './components/task-list/task-list';
 import { CreateTaskComponent } from './components/create-task/create-task';
 import { AssignmentComponent } from './components/assign/assign';
+import { authGuard } from './auth-guard';
 
 export const routes: Routes = [
     { path: '', redirectTo:'login', pathMatch:'full'},
     { path: 'login',  component: LoginComponent},
     { path: 'register', component: RegisterComponent},
-    { path: 'tasks/new', component: CreateTaskComponent},
-    { path: 'tasks/:id/assign', component: AssignmentComponent},
+    { path: 'tasks/new', component: CreateTaskComponent, canActivate: [authGuard]},
+    { path: 'tasks/:id/assign', component: AssignmentComponent, canActivate: [authGuard]},
     { path: 'tasks',  children: [
-        { path: 'in-progress', component: TaskListComponent},
-        { path: 'backlog', component: TaskListComponent},
-        { path: 'completed', component: TaskListComponent}
+        { path: 'in-progress', component: TaskListComponent, canActivate: [authGuard]},
+        { path: 'backlog', component: TaskListComponent, canActivate: [authGuard]},
+        { path: 'completed', component: TaskListComponent, canActivate: [authGuard]}
     ]}
     
 ];
